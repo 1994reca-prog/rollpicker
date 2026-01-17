a478a34e45c0c39945055d04549e42571aaa53e5
1994reca-prog <1994reca@gmail.com>
2026-01-17 14:20:58 +0500

    Create MainActivity.java

---
+++ app/app/MainActivity.java
@@ -0,0 +1,25 @@
+package com.rollpicker.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        // Загружаем локальное HTML-приложение
+        webView.loadUrl("file:///android_asset/index.html");
+
+        setContentView(webView);
+    }
+}
